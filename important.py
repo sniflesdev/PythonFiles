45c692b6197f51c1fb62f11e9294f707357d1791
snifles <nissehanel09@cock.li>
2021-04-03 23:01:33 +0200

    Datei Hinzugefügt

---
+++ important.py
@@ -0,0 +1,27 @@
+def einzelstuecke():
+    for c in "python":
+        print(c)
+
+def zeilenabstand123():
+    for i in [1,2,3]:
+        print(i)
+
+# Funktion um die ungeraden zahlen von 1..10 zu sehen
+def ungeradevoneinsbiszehn():
+    for i in range(1,10,2):
+        print(i)
+def fortest():
+    for i in range(0,100,2):
+        print(i)
+
+
+
+# Funktion um unendlich mal "test" zu printen
+
+x = 4
+def unendlichprint():
+    while x < 10:
+        print("test")
+        
+
+
